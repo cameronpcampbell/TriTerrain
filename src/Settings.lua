d3a8a3aac9882ebe35ed6c68108bd89c8f806d45
MightyPart <cameron.campbell325@gmail.com>
2022-10-17 00:43:17 +0100

    Create Settings.lua

---
+++ src/Settings.lua
@@ -0,0 +1,7 @@
+local Settings = {}
+
+Settings.WIDTH, Settings.HEIGHT, Settings.DEPTH, Settings.SCALE = 20, 20, 20, 5
+Settings.SEED, Settings.ISOVALUE = 50, 0
+Settings.SMOOTH = true
+
+return Settings
